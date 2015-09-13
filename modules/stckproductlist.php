1131a55e8534faab4574e42767a047eedde067c7
Grzegorz Cichowski <gcichowski@gmail.com>
2015-09-13 10:54:18 +0200

    poprawki w liscie produktow

--- modules/stckproductlist.php
+++ modules/stckproductlist.php
@@ -8,30 +8,29 @@ else
 
 $SESSION->save('sglo', $o);
 
-//$grouplist = $LMSST->GroupGetList($o);
-//$grouplist = $DB->GetAll('SELECT * FROM `stck_products`');
-$grouplist = $DB->GetAll('SELECT id AS gid, name AS gname, comment AS gcomment
+//$productlist = $LMSST->ProductGetList($o);
+
+// dodac wyciaganie il. szt. danego produktu oraz ich wartosc
+$productlist = $DB->GetAll('SELECT id AS gid, name AS gname, comment AS gcomment
         FROM stck_products
         WHERE deleted = 0');
 
-$grouplist['total'] = sizeof($grouplist);
-$grouplist['order'] = $order;
-$grouplist['direction'] = $direction;
+$productlist['total'] = sizeof($productlist);
+$productlist['order'] = $order;
+$productlist['direction'] = $direction;
 
-
-
-$listdata['total'] = $grouplist['total'];
-$listdata['direction'] = $grouplist['direction'];
-$listdata['order'] = $grouplist['order'];
-unset($grouplist['total']);
-unset($grouplist['direction']);
-unset($grouplist['order']);
+$listdata['total'] = $productlist['total'];
+$listdata['direction'] = $productlist['direction'];
+$listdata['order'] = $productlist['order'];
+unset($productlist['total']);
+unset($productlist['direction']);
+unset($productlist['order']);
 
 if(!isset($_GET['page']))
         $SESSION->restore('sglp', $_GET['page']);
 
 $page = (! $_GET['page'] ? 1 : $_GET['page']);
-$pagelimit = (! $CONFIG['phpui']['grouplist_pagelimit'] ? $listdata['total'] : $CONFIG['phpui']['grouplist_pagelimit']);
+$pagelimit = (! $CONFIG['phpui']['productlist_pagelimit'] ? $listdata['total'] : $CONFIG['phpui']['productlist_pagelimit']);
 $start = ($page - 1) * $pagelimit;
 
 $SESSION->save('swlp', $page);
@@ -41,7 +40,7 @@ $SESSION->save('backto', $_SERVER['QUERY_STRING']);
 $SMARTY->assign('page',$page);
 $SMARTY->assign('pagelimit',$pagelimit);
 $SMARTY->assign('start',$start);
-$SMARTY->assign('grouplist', $grouplist);
+$SMARTY->assign('productlist', $productlist);
 $SMARTY->assign('listdata', $listdata);
 $SMARTY->display('stck/stckproductlist.html');
 ?>
